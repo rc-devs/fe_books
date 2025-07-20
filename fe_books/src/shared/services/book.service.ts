ed3b1ec110665811c79c0fdecd6e20dbed4c6d93
rc <rc.devs9@gmail.com>
2025-07-20 09:47:58 -0500

    import httpclient

--- fe_books/src/shared/services/book.service.ts
+++ fe_books/src/shared/services/book.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class BookService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
 
   private url = 'http://localhost:3000/books'
 
