2b40c6a25594e50a4c3958e87336470984539e5b
rc-devs <rc.devs9@gmail.com>
2025-07-20 09:28:50 -0500

    placeholder methods for CRUD

--- fe_books/src/shared/services/book.service.ts
+++ fe_books/src/shared/services/book.service.ts
@@ -6,4 +6,20 @@ import { Injectable } from '@angular/core';
 export class BookService {
 
   constructor() { }
+
+  createBook(){
+
+  }
+
+  showBook(){
+
+  }
+
+  updateBook(){
+
+  }
+
+  deleteBook(){
+    
+  }
 }
