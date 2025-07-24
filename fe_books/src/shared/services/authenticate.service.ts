6b23251aca00b87aa7c898732f8f13e313852db2
rc-devs <rc.devs9@gmail.com>
2025-07-24 18:57:12 -0500

    router/http variables, tokenSubject

--- fe_books/src/shared/services/authenticate.service.ts
+++ fe_books/src/shared/services/authenticate.service.ts
@@ -1,11 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
+  private readonly tokenSubject = new BehaviorSubject<string | null>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router ) {}
 
   login(){}
 
