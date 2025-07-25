efbc69645c401f08d7026cb316ef6d39e18c9f45
rc <rc.devs9@gmail.com>
2025-07-25 13:11:10 -0500

    router outlet and routermodule for display booklist path

--- fe_books/src/app/core/navbar/navbar.component.ts
+++ fe_books/src/app/core/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [LoginComponent],
+  imports: [LoginComponent, RouterOutlet, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
