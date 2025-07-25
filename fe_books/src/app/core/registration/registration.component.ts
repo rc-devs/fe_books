f5cd3ee6d991c91b4fad4e911301ae439d582b16
rc <rc.devs9@gmail.com>
2025-07-25 15:25:03 -0500

    logic for registration, + alert for reg service not created

--- fe_books/src/app/core/registration/registration.component.ts
+++ fe_books/src/app/core/registration/registration.component.ts
@@ -8,9 +8,16 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  //constructor(private regService: RegistrationService){}
+
 
   registrationForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required])
   })
+
+  registrationHandler(){
+   alert("The regisration service has not yet been created")
+    // this.regService.registerUser(this.registrationForm.controls.username!, this.registrationForm.controls.password!)
+  }
 }
