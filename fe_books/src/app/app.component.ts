1d71dadaebdbdca311daa3cb80c1972172794a42
rc <rc.devs9@gmail.com>
2025-07-24 19:37:24 -0500

    login component for testing

--- fe_books/src/app/app.component.ts
+++ fe_books/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookListComponent } from './features/book-list/book-list.component';
+import { LoginComponent } from './core/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BookListComponent],
+  imports: [RouterOutlet, BookListComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
