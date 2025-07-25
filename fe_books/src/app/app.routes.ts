557e81172bf17059a10525fe8d04d4f3d67bbcbd
rc <rc.devs9@gmail.com>
2025-07-25 13:02:44 -0500

    add book-list route, protect with authguard

--- fe_books/src/app/app.routes.ts
+++ fe_books/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '../shared/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./core/login/login.component').then((c) => c.LoginComponent),
-    // canActivate: [authGuard]
-  }
+    loadComponent: () => import('./core/login/login.component').then((c) => c.LoginComponent) 
+  },
+  {
+    path: 'book-list',
+    loadComponent: () => import('./features/book-list/book-list.component').then((c) => c.BookListComponent),
+    canActivate: [authGuard]
+  },
 ];
