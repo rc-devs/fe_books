27bfcaeb0ba519f98b26ab5865917261be5c7b9c
rc <rc.devs9@gmail.com>
2025-07-24 20:17:59 -0500

    remove unused imports

--- fe_books/src/app/app.component.ts
+++ fe_books/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BookListComponent } from './features/book-list/book-list.component';
-import { LoginComponent } from './core/login/login.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 
 @Component({
