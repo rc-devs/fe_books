0f4e75dd867cc967a44bb421d3fb42d44bc1a870
rc <rc.devs9@gmail.com>
2025-07-25 15:20:11 -0500

    reactive form for registration

--- fe_books/src/app/core/registration/registration.component.ts
+++ fe_books/src/app/core/registration/registration.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
 
+  registrationForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
 }
