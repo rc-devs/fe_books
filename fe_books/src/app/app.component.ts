001e704ffd5609354a735ba8e5562ec53ec6c735
rc <rc.devs9@gmail.com>
2025-07-25 15:09:30 -0500

    conditionally display router, probably not optimal (duplicate login displayed on logout, bc app-navbar is hardcoded, then displayed through router)

--- fe_books/src/app/app.component.ts
+++ fe_books/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/navbar/navbar.component';
+import { AuthenticateService } from '../shared/services/authenticate.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fe_books';
+
+  constructor(public authService: AuthenticateService){}
 }
