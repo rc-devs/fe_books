c9c36823d0c40ab09a5defcc35d397da977ba8d2
rc <rc.devs9@gmail.com>
2025-07-25 15:03:11 -0500

    conditionally display login/logout

--- fe_books/src/app/core/navbar/navbar.component.ts
+++ fe_books/src/app/core/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { LogoutComponent } from '../logout/logout.component';
+import { AuthenticateService } from '../../../shared/services/authenticate.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [LoginComponent, RouterOutlet, RouterModule],
+  imports: [LoginComponent, LogoutComponent, RouterOutlet, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(public authService: AuthenticateService){}
 
+  
 }
