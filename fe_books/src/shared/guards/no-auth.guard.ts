b72f5d2295d98f8b1afc5051887642e18606de10
rc <rc.devs9@gmail.com>
2025-07-25 13:06:32 -0500

    if logged in, prevent access. if logged out, allow access

--- fe_books/src/shared/guards/no-auth.guard.ts
+++ fe_books/src/shared/guards/no-auth.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthenticateService } from '../services/authenticate.service';
 
 export const noAuthGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthenticateService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()){
+    console.log('User cannont access this path if logged in')
+    return false;
+  } else {
+    return true;
+  }
 };
