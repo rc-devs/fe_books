935bd026bcc48e3caa4e19996fd62048aa7e600d
rc <rc.devs9@gmail.com>
2025-07-24 19:50:11 -0500

    add route for login

--- fe_books/src/app/app.routes.ts
+++ fe_books/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./core/login/login.component').then((c) => c.LoginComponent),
+    // canActivate: [authGuard]
+  }
+];
