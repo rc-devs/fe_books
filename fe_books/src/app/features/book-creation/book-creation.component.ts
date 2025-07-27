6bcd737e3341c71042e95ffe75360c9337d6eed3
rc <rc.devs9@gmail.com>
2025-07-26 21:06:34 -0500

    snackbar!

--- fe_books/src/app/features/book-creation/book-creation.component.ts
+++ fe_books/src/app/features/book-creation/book-creation.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BookService } from '../../../shared/services/book.service';
 import { Router } from '@angular/router';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-book-creation',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatSnackBarModule],
   templateUrl: './book-creation.component.html',
   styleUrl: './book-creation.component.css'
 })
 export class BookCreationComponent {
   
-  constructor(private bookService: BookService, private router:Router){}
+  constructor(private bookService: BookService, private router:Router, private snackBar: MatSnackBar){}
 
   newBook = new FormGroup({
     title: new FormControl("", Validators.required), 
@@ -33,7 +35,8 @@ export class BookCreationComponent {
           //wait till createbook before reload
           this.bookService.showAllBooks; //reload list (could be problem if many book)
           this.newBook.reset(); //reset form
-          this.router.navigate(['/book-list']);
+          this.router.navigate(['/book-list']); //remove once nav button to index/booklist added
+          this.snackBar.open(`${book.title} by ${book.author} has been successfully added to your list.`, 'Close')
         }
         // could suscribe to errors i guess
       });
