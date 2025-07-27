925d4e2970296c63877b9acbc513d4160c57106b
rc <rc.devs9@gmail.com>
2025-07-27 14:27:09 -0500

    hardcode signup below login, remove button

--- fe_books/src/app/core/navbar/navbar.component.ts
+++ fe_books/src/app/core/navbar/navbar.component.ts
@@ -3,10 +3,11 @@ import { LoginComponent } from '../login/login.component';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { LogoutComponent } from '../logout/logout.component';
 import { AuthenticateService } from '../../../shared/services/authenticate.service';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [LoginComponent, LogoutComponent, RouterOutlet, RouterModule],
+  imports: [LoginComponent, LogoutComponent, SignupComponent, RouterOutlet, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
