1f543b85f780be7861042b210d0e8b73ad50d621
rc <rc.devs9@gmail.com>
2025-07-27 13:57:09 -0500

    import auth service, create basic logic for handler, minor syntax in form

--- fe_books/src/app/core/signup/signup.component.ts
+++ fe_books/src/app/core/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthenticateService } from '../../../shared/services/authenticate.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,10 +10,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SignupComponent {
 
+  constructor(private authService: AuthenticateService){}
+
   signUpForm = new FormGroup({
-    username: new FormControl([Validators.required]),
-    password: new FormControl([Validators.required]),
-    password_confirmation: new FormControl([Validators.required]),
+    username: new FormControl("", [Validators.required]),
+    password: new FormControl("",[Validators.required]),
+    password_confirmation: new FormControl("", [Validators.required]),
   })
 
+  signUpHandler(){
+    this.authService.signUp(this.signUpForm.value.username!, this.signUpForm.value.password!, this.signUpForm.value.password_confirmation!).subscribe()
+  }
 }
