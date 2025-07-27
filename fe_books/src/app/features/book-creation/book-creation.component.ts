5936b43040571849c103e0a891c906094fed035f
rc <rc.devs9@gmail.com>
2025-07-26 20:00:43 -0500

    gen book-creation component

---
+++ fe_books/src/app/features/book-creation/book-creation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-creation',
+  imports: [],
+  templateUrl: './book-creation.component.html',
+  styleUrl: './book-creation.component.css'
+})
+export class BookCreationComponent {
+
+}
