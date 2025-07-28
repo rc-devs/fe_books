365c6d6aa44ca4e93e0bcb5c6abaaf8639e80564
rc <rc.devs9@gmail.com>
2025-07-28 18:41:30 -0500

    navbar always present, display other components via router-outlet

--- fe_books/src/app/core/layout/layout.component.ts
+++ fe_books/src/app/core/layout/layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [NavbarComponent, RouterOutlet, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
