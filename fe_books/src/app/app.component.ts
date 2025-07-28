6b45268c28cc5e66b77585225fd30a36751f5421
rc <rc.devs9@gmail.com>
2025-07-28 18:41:04 -0500

    conditionally display layout or login

--- fe_books/src/app/app.component.ts
+++ fe_books/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { AuthenticateService } from '../shared/services/authenticate.service';
+import { LayoutComponent } from './core/layout/layout.component';
+import { LoginComponent } from './core/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, RouterModule],
+  imports: [LayoutComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
