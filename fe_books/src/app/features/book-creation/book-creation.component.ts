16a8d15df3e8a102685188c05752ad1ad338191d
rc <rc.devs9@gmail.com>
2025-08-07 19:03:07 -0500

    format

--- fe_books/src/app/features/book-creation/book-creation.component.ts
+++ fe_books/src/app/features/book-creation/book-creation.component.ts
@@ -1,61 +1,71 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { BookService } from '../../../shared/services/book.service';
 import { Router } from '@angular/router';
-import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
-
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-book-creation',
   imports: [ReactiveFormsModule, MatSnackBarModule],
   templateUrl: './book-creation.component.html',
-  styleUrl: './book-creation.component.css'
+  styleUrl: './book-creation.component.css',
 })
 export class BookCreationComponent {
-  
-  constructor(private bookService: BookService, private router:Router, private snackBar: MatSnackBar){}
+  constructor(
+    private bookService: BookService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   selectedFile: File | null = null;
 
   newBook = new FormGroup({
-    title: new FormControl("", Validators.required), 
-    author: new FormControl("", Validators.required),
-    read: new FormControl(false)
+    title: new FormControl('', Validators.required),
+    author: new FormControl('', Validators.required),
+    read: new FormControl(false),
   });
 
-   onFileSelected(event: any) {
+  onFileSelected(event: any) {
     if (event.target.files && event.target.files[0]) {
       this.selectedFile = event.target.files[0];
     }
-  };
+  }
 
   createHandler() {
-      console.log('createhandler fires');
-      
-      //save form inputs for passage to methods
-      let book = {
-        title: this.newBook.value.title!,
-        author: this.newBook.value.author!,
-        read: this.newBook.value.read!,
-      };
+    console.log('createhandler fires');
 
-      if (this.newBook.valid && this.selectedFile){
-        const formData = new FormData();
-        formData.append('cover_image', this.selectedFile, this.selectedFile.name);
+    //save form inputs for passage to methods
+    let book = {
+      title: this.newBook.value.title!,
+      author: this.newBook.value.author!,
+      read: this.newBook.value.read!,
+    };
 
-        this.bookService.createBook(book).subscribe({
+    if (this.newBook.valid && this.selectedFile) {
+      const formData = new FormData();
+      formData.append('cover_image', this.selectedFile, this.selectedFile.name);
+
+      this.bookService.createBook(book).subscribe({
         next: (createBook) => {
           //wait till createbook before reload
           this.bookService.showAllBooks; //reload list (could be problem if many book)
           this.newBook.reset(); //reset form
-          this.snackBar.open(`${book.title} by ${book.author} has been successfully added to your list.`, 'Close', {duration: 5000})
+          this.snackBar.open(
+            `${book.title} by ${book.author} has been successfully added to your list.`,
+            'Close',
+            { duration: 5000 }
+          );
         },
         // suscribe to errors i guess
         error: (error: any) => {
-          this.snackBar.open('Error creating book','Close', error); // no duration so user can see
-      }});
-      }
-
-      
-    };
+          this.snackBar.open('Error creating book', 'Close', error); // no duration so user can see
+        },
+      });
+    }
+  }
 }
